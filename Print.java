364fd821218b9b3e6de994ceb75a68e9d22388a8
Arghali123 <arghalidaenish@gmail.com>
2025-01-23 22:09:56 +0545

    First Commit

---
+++ Print.java
@@ -0,0 +1,9 @@
+public class Print
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Daenish");
+        System.out.println("Ashik");
+        System.out.println("Ayush");
+    }
+}
