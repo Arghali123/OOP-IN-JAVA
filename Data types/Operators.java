febbb493de827f63a8795a5ecb0a14a3a032a4cb
Arghali123 <arghalidaenish@gmail.com>
2025-01-27 11:44:02 +0545

    next day of java

---
+++ Data types/Operators.java
@@ -0,0 +1,18 @@
+public class Operators {
+    public static void main(String[] args) {
+        //Operators:Operators are symbol used to perform oprations(+,-,*,/)etc.
+        //Operands:Operands are the variables.
+        //(a+b):a and b are operands + is operator.
+
+        //1.) Arthematic Operator: Performs mathematic Calculations(+,-,*,/).
+        int a=20;
+        int b=60;
+        int addresult=a+b;
+        int subresult=a-b;
+        double divresult=(a/b);
+        System.out.println("Addition: "+addresult);
+        System.out.println("Subtraction: "+subresult);
+        System.out.println("Division: "+divresult);
+
+    }
+}
